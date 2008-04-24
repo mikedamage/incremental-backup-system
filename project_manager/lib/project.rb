c13ba3331dba54b18066ccd1ad746c1e002900b1
Mike Green <mike@fifthroomcreative.com>
2008-04-24 15:02:28 -0400

    renamed the merb based web interface directory to make room for a rails interface

--- project_manager/lib/project.rb
+++ project_manager/lib/project.rb
@@ -9,19 +9,15 @@ class Project < ActiveRecord::Base
 	include ProjectManager
 	
 	def self.update_active_from_filesystem
-		@active_path = Pathname.new(ProjectManager::ACTIVE_PROJECTS_DIR)
+		@active_projects = Dir.entries(ProjectManager::ACTIVE_PROJECTS_DIR).delete_if { |x| x == "." || x == ".." }
 		
-		@active_path.children.each do |project| # list all the active projects
-			project.children.each do |file|				# open up project folders
-				if file.to_s =~ /\.yml$/ 						# check for a YAML file in each project's root directory, parse if exists
-					info = YAML.load_file(file.to_s)
-					query = self.find(:all, :conditions => ["name = ?", info['name']]) # return an array with projects with matching names
+		@active_projects.each do |project|	# list all the active projects
+			syspath = File.expand_path(project)
+			if File.directory?(syspath)
+				if FileTest.exists?(File.join(syspath, "info.yml"))
 					
-					unless query.empty? # if the project exists already, just update the details
-						
-					else								# otherwise just create a new one
-						
-					end	
+				else
+					
 				end
 			end
 		end
