c085f3e7706bba85b64325c2e2280bb8d947ca99
Mike Green <mike.is.green@gmail.com>
2008-05-23 02:37:25 -0400

    changed the scheduler script to a Daemonize.run_proc

--- bin/scheduler.rb
+++ bin/scheduler.rb
@@ -7,39 +7,53 @@
 # & Backups from the profiles in config/backup_manager.yml
 
 require 'rubygems'
+require 'daemons'
 require 'openwfe/util/scheduler'
-require '/Users/mike/Development/nibs/lib/backup.rb'
+require File.join(File.dirname(File.expand_path(__FILE__)), "../lib/backup.rb")
 
 include Backup
 include OpenWFE
 
-scheduler = Scheduler.new
-scheduler.start
-SCHEMAS.each do |schema|
-	settings = settings(schema)
-	interval = hourly_snapshot_interval(schema)
-	unless settings['source'].nil?
-		unless settings['hourly'].nil? || settings['snapdir'].nil?
-			scheduler.schedule_every("#{interval}h", :tags => ['snapshot', 'hourly', schema]) do
-				snapshot = Snapshot.new(schema, "hourly")
-				snapshot.snap
-			end
-		end	
-		unless settings['daily'].nil? || settings['backupdir'].nil?
-			scheduler.schedule_every("24h", :tags => ['snapshot', 'daily', schema]) do
-				snapshot = Snapshot.new(schema, "daily")
-				snapshot.snap
+APP_DIR = File.join(File.dirname(File.expand_path(__FILE__)), "..")
+
+Daemons.run_proc(
+	'nibs_scheduler',
+	:dir_mode => :normal,
+	:dir => File.join(APP_DIR, "log"),
+	:multiple => false,
+	:backtrace => true,
+	:monitor => true,
+	:log_output => true) do
+
+	scheduler = Scheduler.new
+	scheduler.start
+	SCHEMAS.each do |schema|
+		settings = settings(schema)
+		interval = hourly_snapshot_interval(schema)
+		unless settings['source'].nil?
+			unless settings['hourly'].nil? || settings['snapdir'].nil?
+				puts schema
+				scheduler.schedule_every("#{interval}h", :tags => ['snapshot', 'hourly', schema]) do
+					snapshot = Snapshot.new(schema, "hourly")
+					snapshot.snap
+				end
+			end	
+			unless settings['daily'].nil? || settings['backupdir'].nil?
+				scheduler.schedule_every("24h", :tags => ['snapshot', 'daily', schema]) do
+					snapshot = Snapshot.new(schema, "daily")
+					snapshot.snap
+				end
 			end
 		end
 	end
-end
-# Aligns this script's thread with that of the scheduler (script runs until the scheduler is stopped).
-scheduler.join 
+	# Aligns this script's thread with that of the scheduler (script runs until the scheduler is stopped).
+	scheduler.join 
 
 
-# Make sure we shut down politely:
-Signal.trap("TERM") do
-#at_exit do
-	scheduler.stop
-	exit
+	# Make sure we shut down politely:
+	#Signal.trap("TERM") do
+	at_exit do
+		scheduler.stop
+		puts "Scheduler Stopped."
+	end
 end
