770e62c70161709057aea191dc7c07923bf0cfdb
Mike Green <mike.is.green@gmail.com>
2008-08-20 02:16:07 -0400

    added Snapshot constructor and parent module called Backup

--- refactor/lib/backup.rb
+++ refactor/lib/backup.rb
@@ -4,17 +4,18 @@ require 'yaml'
 require 'pathname'
 require 'rubygems'
 
-class Backup
-	@@settings = YAML.load_file(File.join(File.dirname(__FILE__), "../config/settings.yml"))
-	@@profiles = @@settings['BackupProfiles']
-	
-	attr_reader :profile
-	
-	def initiailze(profile)
-		@profile 	= profile
-		@settings = @@profiles[@profile]
-		@source		= Pathname.new(@settings['Source'])
-	end
-	
-	
+module Backup
+	SETTINGS = YAML.load_file(File.join(File.dirname(__FILE__), "../config/config.yml"))
+	PROFILES = SETTINGS['backup_profiles']	
 end
+
+require File.join(File.expand_path(File.dirname(__FILE__)), 'snapshot.rb')
+
+# Pathname.new(File.expand_path(File.dirname(__FILE__))).children.each do |file|
+# 	filename = file.to_s
+# 	
+# 	if filename != "backup.rb" && filename =~ /\.rb$/
+# 		require filename
+# 	end
+# 	
+# end
--- refactor/lib/snapshot.rb
+++ refactor/lib/snapshot.rb
@@ -1,10 +1,15 @@
-class Snapshot < Backup
-	attr_reader :profile, :interval
+module Backup
+	class Snapshot
+		attr_reader :profile, :interval
 	
-	def initialize(profile, interval)
-		super(profile)
-		@interval 		= interval
-		@destination	= Pathname.new(@settings['SnapshotDir'] + "/#{@interval}")
+		def initialize(profile)
+			@profile			= PROFILES[profile]
+			@snap_prefs		= @profile['snapshots']
+			@interval 		= @snap_prefs['interval']
+			@destination	= Pathname.new(@snap_prefs['storage'])
+			@snapshots		= @destination.children.sort {|x,y| x.ctime <=> y.ctime }
+			@latest_snap	= @snapshots.last
+		end
+	
 	end
-	
 end
